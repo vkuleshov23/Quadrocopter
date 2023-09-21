0a7ea3172f17cd82c97d2ab039533defb6f4688e
vkuleshov23 <kvs23_06_02@mail.ru>
2023-09-21 18:16:31 +0300

    nrf test

--- RaspberryController/compile.sh
+++ RaspberryController/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull
+
 #expecting BCM2708 or BCM2709.
 #If this is a genuine Raspberry Pi then please report this to projects@drogon.net.
 #If this is not a Raspberry Pi then you are on your own as wiringPi is designed to support the Raspberry Pi.
