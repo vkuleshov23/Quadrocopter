d100d0bdb61bbcfd78584d64574627db9c00560a
vkuleshov23 <kvs23_06_02@mail.ru>
2023-09-21 17:43:22 +0300

    nrf test

---
+++ RaspberryController/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#expecting BCM2708 or BCM2709.
+#If this is a genuine Raspberry Pi then please report this to projects@drogon.net.
+#If this is not a Raspberry Pi then you are on your own as wiringPi is designed to support the Raspberry Pi.
+#then use this int console
+export JAVA_TOOL_OPTIONS=-Dpi4j.linking=dynamic
+
+mvn spring-boot:run
