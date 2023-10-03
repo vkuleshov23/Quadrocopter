0eab2c5ca39143db0d5409723a26f5ebff638f83
vkuleshov23 <kvs23_06_02@mail.ru>
2023-10-03 16:23:04 +0300

    madgwick

--- RaspberryController/src/main/java/pi/quadrocopter/configuration/NRFConfig.java
+++ RaspberryController/src/main/java/pi/quadrocopter/configuration/NRFConfig.java
@@ -13,11 +13,13 @@ public class NRFConfig {
 
     private static final byte[] writingAddress = {(byte)0xAA, (byte)0xAA, (byte) 0xAA, (byte) 0xAA, (byte) 0xAA};
     private static final byte[] readingAddress = {(byte)0xFF, (byte)0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+    private final long SAMPLE_MS = 50;
 
     @Bean
     @Scope("singleton")
     public NRF24 nrf() throws IOException {
         NRF24 nrf = new NRF24();
+        nrf.setSampleMS(SAMPLE_MS);
         nrf.begin();
         nrf.setChannel(76);
         nrf.setCRCLength(NRF24.RF24_CRC_16);
@@ -28,7 +30,6 @@ public class NRFConfig {
 //        nrf.openWritingPipe(writingAddress);
         nrf.openReadingPipe(1, readingAddress);
         nrf.startListening();
-        nrf.setSampleMS(50);
         return nrf;
     }
 }
