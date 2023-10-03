aa3956157e21af07f632e741a30b807712cfa9cd
vkuleshov23 <kvs23_06_02@mail.ru>
2023-10-03 20:59:05 +0300

    madgwick

--- RaspberryController/src/main/java/pi/quadrocopter/service/SensorDataCollector.java
+++ RaspberryController/src/main/java/pi/quadrocopter/service/SensorDataCollector.java
@@ -64,7 +64,7 @@ public class SensorDataCollector {
 
         synchronized (averageData) {
             ArrayList<Float> a = new ArrayList<>(3);
-            averageData.add(new ThreeAxes(accAxes));
+            averageData.add(new ThreeAxes(magAxes));
         }
     }
 
